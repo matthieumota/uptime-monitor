66a7925b24fc1d5b89eeaee1d5bacef805b42503
Matthieu Mota <matthieumota@gmail.com>
2019-07-06 15:16:59 +0200

    Init

---
+++ README.md
@@ -0,0 +1,39 @@
+UptimeMonitor Component
+=======================
+
+The UptimeMonitor component provides PHP Object Oriented library to easily monitor websites.
+
+## Installation
+
+UptimeMonitor uses Composer.
+
+```
+composer require matthieumota/uptime-monitor
+```
+
+## Usage
+
+```php
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+use MatthieuMota\Component\UptimeMonitor\UptimeMonitor;
+
+$uptimeMonitor = new UptimeMonitor();
+
+$uptimeMonitor
+	->add('https://boxydev.com')
+    ->add('https://matthieumota.fr')
+    ->add('https://domain.notexists')
+    ->add('https://expired.badssl.com')
+;
+
+// Simply stdout result at the moment
+$uptimeMonitor->check();
+
+```
+
+## Notes
+
+This component is work in progress, it's no stable. Also, you can contribute, a PR well documented and tested can be merged.
---
+++ src/UptimeMonitor.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace MatthieuMota\Component\UptimeMonitor;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Pool;
+use GuzzleHttp\Psr7\Request;
+use Psr\Http\Message\ResponseInterface;
+
+class UptimeMonitor
+{
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * @var array
+     */
+    private $urls;
+
+    public function __construct(Client $client = null)
+    {
+        $this->client = $client ?? new Client();
+    }
+
+    public function add(string $url): self
+    {
+        $this->urls[] = $url;
+
+        return $this;
+    }
+
+    public function check()
+    {
+        $requests = function () {
+            foreach ($this->urls as $url) {
+                yield new Request('GET', $url);
+            }
+        };
+
+        $pool = new Pool($this->client, $requests(), [
+            'concurrency' => 10,
+            'fulfilled' => function (ResponseInterface $response, int $index) {
+                if (200 === $response->getStatusCode()) {
+                    echo $this->urls[$index].' is up ! ✅'.PHP_EOL;
+                } else {
+                    echo $this->urls[$index].' is down ! ❌ ('.$response->getStatusCode().')'.PHP_EOL;
+                }
+            },
+            'rejected' => function (RequestException $exception, int $index) {
+                echo $this->urls[$index].' is down ! ❌ ('.$exception->getMessage().')'.PHP_EOL;
+            },
+        ]);
+
+        $pool->promise()->wait();
+    }
+}
