89c75a99a43aff4c81f55b0fbcc5dac5bae4334e
Matthieu Mota <matthieumota@gmail.com>
2019-07-06 15:26:41 +0200

    Fix license code

--- src/UptimeMonitor.php
+++ src/UptimeMonitor.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the UptimeMonitor package.
+ *
+ * (c) Matthieu Mota <matthieu@boxydev.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace MatthieuMota\Component\UptimeMonitor;
 
 use GuzzleHttp\Client;
